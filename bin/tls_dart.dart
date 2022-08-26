08239b13d5930b1450a5e4ac2b5adf7c7dc416cb
James Raden <james.raden@gmail.com>
2022-08-25 23:30:19 -0400

    Correct the code so it works w/ httpbin.org

--- bin/tls_dart.dart
+++ bin/tls_dart.dart
@@ -3,11 +3,11 @@ import 'package:tls_dart/tls_dart.dart' as tls_dart;
 import 'package:http/http.dart' as http;
 
 Future<void> main(List<String> arguments) async {
-  var url = Uri.https('example.com', 'whatsit/create');
+  var url = Uri.https('httpbin.org', 'post');
   var response =
       await http.post(url, body: {'name': 'doodle', 'color': 'blue'});
   print('Response status: ${response.statusCode}');
   print('Response body: ${response.body}');
 
-  print(await http.read(Uri.https('example.com', 'foobar.txt')));
+  print(await http.read(Uri.https('httpbin.org', 'get')));
 }
