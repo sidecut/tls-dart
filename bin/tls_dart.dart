97798b9f4fd1294db7c0bd2e8403029fb816294f
James Raden <james.raden@gmail.com>
2022-08-25 23:20:35 -0400

    Use sample http code from https://pub.dev/packages/http

--- bin/tls_dart.dart
+++ bin/tls_dart.dart
@@ -1,5 +1,13 @@
+// ignore: unused_import
 import 'package:tls_dart/tls_dart.dart' as tls_dart;
+import 'package:http/http.dart' as http;
 
-void main(List<String> arguments) {
-  print('Hello world: ${tls_dart.calculate()}!');
+Future<void> main(List<String> arguments) async {
+  var url = Uri.https('example.com', 'whatsit/create');
+  var response =
+      await http.post(url, body: {'name': 'doodle', 'color': 'blue'});
+  print('Response status: ${response.statusCode}');
+  print('Response body: ${response.body}');
+
+  print(await http.read(Uri.https('example.com', 'foobar.txt')));
 }
