b2f12923885d3710cd746afbef995f10fc74b0da
James Raden <james.raden@gmail.com>
2022-08-25 22:56:18 -0400

    Create dart console project

---
+++ bin/tls_dart.dart
@@ -0,0 +1,5 @@
+import 'package:tls_dart/tls_dart.dart' as tls_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tls_dart.calculate()}!');
+}
---
+++ test/tls_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:tls_dart/tls_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
