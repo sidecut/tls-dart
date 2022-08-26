bed353122b78538f98b54d0e99a3a8a40ca4d159
James Raden <james.raden@gmail.com>
2022-08-26 00:17:17 -0400

    Use client.getUrl

--- bin/tls_dart.dart
+++ bin/tls_dart.dart
@@ -5,14 +5,10 @@ import 'dart:io';
 Future<void> main(List<String> arguments) async {
   var client = HttpClient();
   try {
-    HttpClientRequest request =
-        await client.get('httpbin.org', 80, '/file.txt');
-    // Optionally set up headers...
-    // Optionally write to the request object...
-    HttpClientResponse response = await request.close();
-    // Process the response
-    final stringData = await response.transform(utf8.decoder).join();
-    print(stringData);
+    var request = await client.getUrl(Uri.https('httpbin.org', 'get'));
+    var response = await request.close();
+    print('${response.statusCode}\n');
+    print('$response\n');
   } finally {
     client.close();
   }
