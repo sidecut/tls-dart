c64495b8c5540e61df3e711ad794882d2cea2c84
James Raden <james.raden@gmail.com>
2022-08-26 00:37:32 -0400

    Consume the body, allowing the app to terminate

--- bin/tls_dart.dart
+++ bin/tls_dart.dart
@@ -7,8 +7,13 @@ Future<void> main(List<String> arguments) async {
   try {
     var request = await client.getUrl(Uri.https('httpbin.org', 'get'));
     var response = await request.close();
-    print('${response.statusCode}\n');
-    print('$response\n');
+    print('${response.statusCode}');
+    print('${response.headers.contentType}');
+    print('${response.certificate?.pem}');
+    print("");
+
+    var body = await response.transform(utf8.decoder).join();
+    print(body);
   } finally {
     client.close();
   }
